95b9381be0022b642eda24435ef8b672b4b6d3ff
Dinesh <Dineshnariyani20@gmail.com>
2021-04-04 10:32:05 +0530

    Deployment

--- Fantom/urls.py
+++ Fantom/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('contact/', include('contact.urls')),
     path('archive/', include('myarchive.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
