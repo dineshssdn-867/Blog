c0479ba92e2c2ea9f6ac8a535cfd3c6b7a452fe2
Dinesh <Dineshnariyani20@gmail.com>
2021-04-24 21:23:21 +0530

    added routers file to connect to multiple database

--- Fantom/settings.py
+++ Fantom/settings.py
@@ -209,4 +209,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 django_heroku.settings(locals())
 
-DATABASE_ROUTERS = ['routers.db_routers.AuthRouter','routers.db_routers.Blue','routers.db_routers.Aqua']
+DATABASE_ROUTERS = ['routers.db_routers.AuthRouter','routers.db_routers.posts','routers.db_routers.myarchive']
