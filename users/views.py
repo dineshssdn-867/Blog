1e7257fa1fa0f92fda6590e57463c8ee90629463
dineshssdn-867 <66898317+dineshssdn-867@users.noreply.github.com>
2021-07-12 23:17:58 +0530

    Added delete view

--- users/views.py
+++ users/views.py
@@ -28,6 +28,10 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 
 
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+
 @method_decorator(vary_on_headers('User-Agent', 'Cookie'), name='dispatch')
 @method_decorator(cache_page(60 * 1, cache="special_cache"), name='dispatch')
 class RegisterView(SuccessMessageMixin, CreateView):
@@ -143,3 +147,22 @@ class UserListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(UserListView, self).get_context_data(**kwargs)
         return context
+
+
+@method_decorator(login_required(login_url='/users/login'), name="dispatch")
+class UserDeleteView(DeleteView):
+    template_name = 'users/delete.html'
+    model = User
+    success_url = "/"
+
+    def form_valid(self, form):
+        username = form['username'].value()  # getting the email from form object
+        password = form['password'].value()  # getting the password from form object
+        email = User.objects.filter(username=username).values('email')
+        try:  # some basic validation of e-mail
+            user = auth.sign_in_with_email_and_password(email[0]['email'], password)  # login with e-mail and password
+            auth.delete_user_account(user['idToken'])
+        except:
+            messages.error(self.request,
+                           'Please check your password and e-mail')  # adding the errors in messages list which will be shown in message.html template
+            return HttpResponseRedirect(reverse('users:delete'), kwargs={'pk': self.request.user})  # Redirecting to form page if there are any errors
